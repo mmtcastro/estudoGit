d6b42dc2bcdc7d28ebaae87a7f814a90bcd6ade4
Marcelo Castro <mcastro@tdec.com.br>
2021-06-09 17:13:01 -0300

    Commit Inicial Local

---
+++ estudoGit/src/main/java/br/com/tdec/estudoGit/EstudoGitApplication.java
@@ -0,0 +1,13 @@
+package br.com.tdec.estudoGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoGitApplication.class, args);
+	}
+
+}
---
+++ estudoGit/src/test/java/br/com/tdec/estudoGit/EstudoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tdec.estudoGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
